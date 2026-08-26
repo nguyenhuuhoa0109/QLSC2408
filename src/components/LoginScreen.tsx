cbca9e6babe289cdd3cc5ae64a195f9330013687
nguyenhuuhoa0109 <nguyenhuuhoa0109@gmail.com>
2026-08-26 14:19:45 +0700

    feat(ui): update login screen background image
    Replace the outdated background image with the latest high-resolution version.

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -3,7 +3,7 @@ import { Mail, Lock, Eye, EyeOff, ArrowRight, Phone, AlertCircle, Shield } from 
 import { DatPhuongLogo } from './DatPhuongLogo';
 import { SupabaseService } from '../services/supabaseService';
 import { User } from '../types';
-import sontraImage from '../assets/images/sontra_actual_dam_1787728548595.jpg';
+import sontraImage from '../assets/images/sontra_goc_real_1787728712101.jpg';
 
 interface LoginScreenProps {
   onLogin: (user: User | string) => void;
